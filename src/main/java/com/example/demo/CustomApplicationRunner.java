8cd50ff4f3c99a50e48707f35de57a51ac697213
jiyong.yoon <jyyoon0615@naver.com>
2024-09-05 15:20:45 +0900

    wcm - insert various workspace's contract

--- src/main/java/com/example/demo/CustomApplicationRunner.java
+++ src/main/java/com/example/demo/CustomApplicationRunner.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
@@ -33,18 +32,17 @@ public class CustomApplicationRunner implements ApplicationRunner {
     private final TemplateContractRepository templateContractRepository;
     private final CompanyRepository companyRepository;
 
-    String userName2 = "유저2";
-    String testUserPassword1 = "테스트패스워드1";
-    String testEmail1 = "test1@email";
+    String adminEmail1 = "admin1@email";
     String adminName1 = "유저1&관리자";
     String adminPassword1 = "관리자비밀번호1";
-    String adminEmail1 = "admin1@email";
-    String userName1 = "유저1";
-    String userName3 = "유저3";
-    String userName4 = "유저4";
-    String userPassword1 = "유저비밀번호1";
+    String userEmail2 = "user2@email";
+    String userName2 = "유저2";
     String userEmail3 = "user3@email";
+    String userName3 = "유저3";
     String userEmail4 = "user4@email";
+    String userName4 = "유저4";
+    String userCommonPassword = "유저비밀번호";
+
     String templateName1 = "탬플릿 이름1";
     String templateName2 = "탬플릿 이름2";
     String templateChangeName1 = "탬플릿 이름수정1";
@@ -52,7 +50,9 @@ public class CustomApplicationRunner implements ApplicationRunner {
 
     public User admin;
     public List<User> userList = new ArrayList<>();
-    public Workspace savedWorkspaceEntity;
+    public Workspace savedWorkspaceEntity1; // 유저 1, 2, 3
+    public Workspace savedWorkspaceEntity2; // 유저 1, 2, 4
+    public Workspace savedWorkspaceEntity3; // 유저 1, 3, 4
     public TemplateContract savedTemplateContractEntity;
     public Company savedCompanyEntity;
 
@@ -81,7 +81,7 @@ public class CustomApplicationRunner implements ApplicationRunner {
                     .totalCost(String.valueOf(RandomIntUtils.getRandomInt(100000000)))
                     .salesProfit(String.valueOf(RandomIntUtils.getRandomInt(100000000)))
                     .contractStepType(RandomEnumUtils.getRandomEnum(ContractStepType.class))
-                    .workspaceCode(savedWorkspaceEntity.getId())
+                    .workspaceCode(savedWorkspaceEntity1.getId())
                     .build();
             contractList.add(contract);
         }
@@ -89,8 +89,17 @@ public class CustomApplicationRunner implements ApplicationRunner {
         bulkInsert.bulkInsertContractList(
                 contractList, savedTemplateContractEntity, userList, savedCompanyEntity);
 
-        System.out.printf("insert finished!! workspace code: %s, adminId: %s, adminPassword: %s%n",
-                savedWorkspaceEntity.getId(), adminEmail1, adminPassword1);
+        System.out.printf("insert finished!! workspace code1: %s, workspace code2: %s, workspace code3: %s%n, " +
+                        "adminId: %s, adminPassword: %s%n" +
+                        "user2: %s, user2Password: %s%n" +
+                        "user3: %s, user3Password: %s%n" +
+                        "user4: %s, user4Password: %s%n",
+                savedWorkspaceEntity1.getId(), savedWorkspaceEntity2.getId(), savedWorkspaceEntity3.getId(),
+                adminEmail1, adminPassword1,
+                userEmail2, userCommonPassword,
+                userEmail3, userCommonPassword,
+                userEmail4, userCommonPassword
+        );
 
 //        List<User> userList = new ArrayList<>();
 
@@ -119,8 +128,8 @@ public class CustomApplicationRunner implements ApplicationRunner {
 
         User user2 = User.builder()
                 .userName(userName2)
-                .userEmail(testEmail1)
-                .userPassword(passwordEncoder.encode(testUserPassword1))
+                .userEmail(userEmail2)
+                .userPassword(passwordEncoder.encode(userCommonPassword))
                 .userRole(UserRole.USER)
                 .build();
         User savedUser2 = userRepository.save(user2);
@@ -128,7 +137,7 @@ public class CustomApplicationRunner implements ApplicationRunner {
         User user3 = User.builder()
                 .userName(userName3)
                 .userEmail(userEmail3)
-                .userPassword(passwordEncoder.encode(userPassword1))
+                .userPassword(passwordEncoder.encode(userCommonPassword))
                 .userRole(UserRole.USER)
                 .build();
         User savedUser3 = userRepository.save(user3);
@@ -136,7 +145,7 @@ public class CustomApplicationRunner implements ApplicationRunner {
         User user4 = User.builder()
                 .userName(userName4)
                 .userEmail(userEmail4)
-                .userPassword(passwordEncoder.encode(userPassword1))
+                .userPassword(passwordEncoder.encode(userCommonPassword))
                 .userRole(UserRole.USER)
                 .build();
         User savedUser4 = userRepository.save(user4);
@@ -152,40 +161,94 @@ public class CustomApplicationRunner implements ApplicationRunner {
                 .build();
         deptRepository.saveAll(Arrays.asList(dept1, dept2));
 
-        Workspace workspace = Workspace.builder()
+        Workspace workspace1 = Workspace.builder()
                 .id("W001" + WorkSpaceCodeGenerator.makeUuid(false))
                 .name("워크스페이스1")
                 .isDeleted(false)
                 .build();
-        Workspace savedWorkspace = workspaceRepository.save(workspace);
-        savedWorkspaceEntity = savedWorkspace;
+        Workspace savedWorkspace1 = workspaceRepository.save(workspace1);
+        savedWorkspaceEntity1 = savedWorkspace1;
 
+        Workspace workspace2 = Workspace.builder()
+                .id("W001" + WorkSpaceCodeGenerator.makeUuid(false))
+                .name("워크스페이스2")
+                .isDeleted(false)
+                .build();
+        Workspace savedWorkspace2 = workspaceRepository.save(workspace2);
+        savedWorkspaceEntity2 = savedWorkspace2;
+
+        Workspace workspace3 = Workspace.builder()
+                .id("W001" + WorkSpaceCodeGenerator.makeUuid(false))
+                .name("워크스페이스3")
+                .isDeleted(false)
+                .build();
+        Workspace savedWorkspace3 = workspaceRepository.save(workspace3);
+        savedWorkspaceEntity3 = savedWorkspace3;
+
+        // workspace1
         WorkspaceUserDept workspaceUserDept1 = WorkspaceUserDept.builder()
-                .workspace(savedWorkspace)
+                .workspace(savedWorkspace1)
                 .user(savedUser1)
                 .build();
         WorkspaceUserDept workspaceUserDept2 = WorkspaceUserDept.builder()
-                .workspace(savedWorkspace)
+                .workspace(savedWorkspace1)
                 .user(savedUser2)
                 .build();
         WorkspaceUserDept workspaceUserDept3 = WorkspaceUserDept.builder()
-                .workspace(savedWorkspace)
+                .workspace(savedWorkspace1)
                 .user(savedUser3)
                 .build();
+        // workspace2
         WorkspaceUserDept workspaceUserDept4 = WorkspaceUserDept.builder()
-                .workspace(savedWorkspace)
-                .user(savedUser4)
+                .workspace(savedWorkspace2)
+                .user(savedUser1)
                 .build();
         WorkspaceUserDept workspaceUserDept5 = WorkspaceUserDept.builder()
-                .workspace(savedWorkspace)
-                .dept(dept1)
+                .workspace(savedWorkspace2)
+                .user(savedUser2)
                 .build();
         WorkspaceUserDept workspaceUserDept6 = WorkspaceUserDept.builder()
-                .workspace(savedWorkspace)
+                .workspace(savedWorkspace2)
+                .user(savedUser4)
+                .build();
+        // workspace3
+        WorkspaceUserDept workspaceUserDept7 = WorkspaceUserDept.builder()
+                .workspace(savedWorkspace3)
+                .user(savedUser1)
+                .build();
+        WorkspaceUserDept workspaceUserDept8 = WorkspaceUserDept.builder()
+                .workspace(savedWorkspace3)
+                .user(savedUser3)
+                .build();
+        WorkspaceUserDept workspaceUserDept9 = WorkspaceUserDept.builder()
+                .workspace(savedWorkspace3)
+                .user(savedUser4)
+                .build();
+
+        // workspace1 dept
+        WorkspaceUserDept workspaceUserDept10 = WorkspaceUserDept.builder()
+                .workspace(savedWorkspace1)
+                .dept(dept1)
+                .build();
+        WorkspaceUserDept workspaceUserDept11 = WorkspaceUserDept.builder()
+                .workspace(savedWorkspace1)
                 .dept(dept2)
                 .build();
+
         workspaceUserDeptRepository.saveAll(Arrays.asList(
-                workspaceUserDept1, workspaceUserDept2, workspaceUserDept3, workspaceUserDept4, workspaceUserDept5, workspaceUserDept6));
+                    workspaceUserDept1,
+                    workspaceUserDept2,
+                    workspaceUserDept3,
+                    workspaceUserDept4,
+                    workspaceUserDept5,
+                    workspaceUserDept6,
+                    workspaceUserDept7,
+                    workspaceUserDept8,
+                    workspaceUserDept9,
+                    workspaceUserDept10,
+                    workspaceUserDept11
+                )
+        );
 
         Company company = Company.builder()
                 .companyName(RandomStringUtils.generateRandomString(10))
@@ -199,7 +262,7 @@ public class CustomApplicationRunner implements ApplicationRunner {
                 .templateName("test_template")
                 .reviewOption(ReviewOption.CUSTOM)
                 .deadLine(12)
-                .workspaceCode(savedWorkspaceEntity.getId())
+                .workspaceCode(savedWorkspaceEntity1.getId())
                 .build();
         savedTemplateContractEntity = templateContractRepository.save(templateContract);
     }
